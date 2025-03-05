22626030d7f61b03f13d4d83c77d14ce180912bd
Daniel Gorodeki <dgorodezki@blackbricks.io>
2025-03-05 14:33:53 +0600

    -

--- main.cpp
+++ main.cpp
@@ -1,42 +1,33 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-
+#define _USE_MATH_DEFINES
 #include <cmath>
-#include <iostream>
-#include <vector>
-
 #include "tty.h"
-using namespace std;
 
+#define PORT 7
 #define CHANNELS 5
-#define BUFFER_SIZE 4
 
-#define M_PI (3.14159265)
-#define TABLE_SIZE (100)
+#define TABLE_SIZE 1024
 
 int main() {
   TTY tty;
   char port_name[32];
-  sprintf_s(port_name, "\\\\.\\COM%d", 7);
-  tty.Connect(port_name, 9600);
+  sprintf_s(port_name, "\\\\.\\COM%d", PORT);
+  tty.Connect(port_name, 128000);
 
   float sine[TABLE_SIZE]{};
-  int sample_phase = 0;
   for (int i = 0; i < TABLE_SIZE; i++) {
-    sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.);
+    sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.0);
   }
 
-  while (1) {
-    float out_data[CHANNELS]{};
+  float out_data[CHANNELS]{};
+  int sample_phase = 0;
+  while (tty.IsOK()) {
     for (int i = 0; i < CHANNELS; ++i) {
-      //out_data[i] = sine[sample_phase];
-      out_data[i] = 1;
+      out_data[i] = sine[sample_phase];
     }
     sample_phase += 1;
     if (sample_phase >= TABLE_SIZE) sample_phase -= TABLE_SIZE;
-    tty.Write(out_data);
+
+    tty.Write(out_data, CHANNELS);
   }
 
   return 0;
